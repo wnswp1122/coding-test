52e3e7f10db82265855f6fc15177d92c5659c84b
wnswp1122 <148195782+wnswp1122@users.noreply.github.com>
2026-03-03 11:21:14 +0900

    Update 이모티콘.py

--- 백준/Gold/14226. 이모티콘/이모티콘.py
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -1,32 +1,35 @@
 from collections import deque
-import sys
 
-input = sys.stdin.readline
-
+MAX = 1001
 S = int(input())
-MAX = 1001  
 
 visited = [[False] * MAX for _ in range(MAX)]
-
 queue = deque()
-queue.append((1, 0, 0)) 
+
+# (screen, clipboard, time)
+queue.append((1, 0, 0))
 visited[1][0] = True
 
 while queue:
-    screen, clip, time = queue.popleft()
+    screen, clipboard, time = queue.popleft()
 
     if screen == S:
         print(time)
         break
 
+    # 1. 복사
     if not visited[screen][screen]:
         visited[screen][screen] = True
         queue.append((screen, screen, time + 1))
 
-    if clip > 0 and screen + clip < MAX and not visited[screen + clip][clip]:
-        visited[screen + clip][clip] = True
-        queue.append((screen + clip, clip, time + 1))
+    # 2. 붙여넣기
+    if clipboard > 0 and screen + clipboard < MAX:
+        if not visited[screen + clipboard][clipboard]:
+            visited[screen + clipboard][clipboard] = True
+            queue.append((screen + clipboard, clipboard, time + 1))
 
-    if screen > 0 and not visited[screen - 1][clip]:
-        visited[screen - 1][clip] = True
-        queue.append((screen - 1, clip, time + 1))
+    # 3. 삭제
+    if screen > 0:
+        if not visited[screen - 1][clipboard]:
+            visited[screen - 1][clipboard] = True
+            queue.append((screen - 1, clipboard, time + 1))
